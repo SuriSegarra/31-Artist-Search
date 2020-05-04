86250f0aeae831cf8ef856182570bb21a6e6cd34
surelis segarra <sdarianne34@gmail.com>
2020-05-04 16:51:48 -0700

    list of artists shows in browser, they are Link is working

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 // import ArtistsViewer from '../../Containers/ArtistsViewer';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchedArtist from '../../components/Search/Search';
+import finedArtists from '../../Containers/ArtistsViewer';
 
 export default function App() {
   return (
     <>
       <Router>
         <Switch>
-          <Route exact path='/artist/:id' component={SearchedArtist}/>
+          <Route exact path='/' component={finedArtists}/>
         </Switch>
       </Router>
     
