9271cc66e43f39db27045dd8bb457d3ea38b862c
surelis segarra <sdarianne34@gmail.com>
2020-05-04 17:35:15 -0700

    created fetch for released albums

--- src/services/artists.js
+++ src/services/artists.js
@@ -6,3 +6,13 @@ export const fetchArtist = (artist) => {
     }));
 
 };
+
+export const fetchRelease = (artistId) => {
+  return fetch(`http://musicbrainz.org/ws/2/release?artist=${artistId}&fmt=json`)
+    .then(res => res.json)
+    .then(res => res.releases.map(release => ({
+      id: release.id,
+      title: release.title,
+      date: release.date
+    })));
+};
