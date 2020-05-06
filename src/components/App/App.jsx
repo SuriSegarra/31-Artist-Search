30cdbd31423aaea4cb317637616877ee4bc141e7
surelis segarra <sdarianne34@gmail.com>
2020-05-05 23:26:59 -0700

    fixed wrong path

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import findedArtist from '../../Containers/ArtistsViewer';
 import Releases from '../../Containers/AlbumViewer';
+import Songs from '../../Containers/SongViewer';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Router>
         <Switch>
           <Route exact path='/artist/:id' component={Releases}/>
+          <Route exact path='/release/:id' component={Songs}/>
           <Route exact path='/' component={findedArtist}/>
         </Switch>
       </Router>
