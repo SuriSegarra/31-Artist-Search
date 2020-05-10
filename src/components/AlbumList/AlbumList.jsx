ab116b07f2aa9c7bdef2ee98d367d6f6bf08d484
surelis segarra <sdarianne34@gmail.com>
2020-05-09 17:43:27 -0700

    albums list css

--- src/components/AlbumList/AlbumList.jsx
+++ src/components/AlbumList/AlbumList.jsx
@@ -2,22 +2,27 @@ import React from 'react';
 import Album from '../Album/Album';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styles from './AlbumList.css';
 
-
-const AlbumList = ({ releases, artist }) => {
+const AlbumList = ({ releases, artist, }) => {
   const albumList = releases.map(release => (
-    <Link key={release.id} to={`/song/${artist}/${release.id}`}>
-      <img src={`http://coverartarchive.org/release/${release.id}/front-250`}/>
+    
+    <Link className={styles.textlink} key={release.id} to={`/song/${artist}/${release.id}`}>
+
+      <img src={`http://coverartarchive.org/release/${release.id}/front-250`} />
       <li>
         <Album {...release}/>
       </li>
     </Link>
 
   ));
-  return (
-    <ul>
-      {albumList}
-    </ul>
+  return ( 
+    <>
+      <h1> Albums</h1>
+      <ul className={styles.List}>
+        {albumList}
+      </ul>
+    </>
   );
 };
 
